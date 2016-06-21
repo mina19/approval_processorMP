16f43a62a1e71c3c4c0e58da2ed9f7a23c47609c
Mina Cho <min-a.cho@ligo.org>
2016-06-21 16:45:46 -0400

    update to parseAlert new_to_preliminary

--- approval_processorMPutils.py
+++ approval_processorMPutils.py
@@ -506,8 +506,20 @@ def parseAlert(alert):
     currentstate = event_dict['currentstate']
     if currentstate=='new_to_preliminary':
         for Check in new_to_preliminary:
-            if eval('{0}(event_dict)'.format(Check))==True:
-                print True
+            eval('{0}(event_dict)'.format(Check))
+            checkresult = event_dict[Check + 'result']
+            if checkresult==None:
+                logger.info('{0} -- {1} -- Added {2} to queueByGraceID.'.format(convertTime(), graceid, Check))
+                print 'Added {0} to queueByGraceID'.format(Check)
+            elif checkresult==False:
+                logger.info('{0} -- {1} -- Failed {2} in currentstate: new_to_preliminary.'.format(convertTime(), graceid, Check))
+                logger.info('{0} -- {1} -- Changing currentstate to rejected.'.format(convertTime(), graceid))
+                print 'Failed in the new_to_preliminary state.'
+                print 'Changing currentstate to rejected.'
+                event_dict['currentstate'] = 'rejected'
+                return
+            elif checkresult==True:
+                print 'Do not need to add {0} to queue'.format(Check)
 
 alert = {u'graceid': u'G184098', u'gpstime': 1126259462.391, u'pipeline': u'CWB', u'group': u'Burst', u'links': {u'neighbors': u'https://gracedb.ligo.org/api/events/G184098/neighbors/', u'files': u'https://gracedb.ligo.org/api/events/G184098/files/', u'log': u'https://gracedb.ligo.org/api/events/G184098/log/', u'tags': u'https://gracedb.ligo.org/api/events/G184098/tag/', u'self': u'https://gracedb.ligo.org/api/events/G184098', u'labels': u'https://gracedb.ligo.org/api/events/G184098/labels/', u'filemeta': u'https://gracedb.ligo.org/api/events/G184098/filemeta/', u'emobservations': u'https://gracedb.ligo.org/api/events/G184098/emobservation/'}, u'created': u'2015-09-14 09:53:51 UTC', u'far': 1.17786e-08, u'instruments': u'H1,L1', u'labels': {u'H1OK': u'https://gracedb.ligo.org/api/events/G184098/labels/H1OK', u'L1OK': u'https://gracedb.ligo.org/api/events/G184098/labels/L1OK'}, u'extra_attributes': {u'MultiBurst': {u'central_freq': 123.828491, u'false_alarm_rate': None, u'confidence': None, u'start_time_ns': 750000000, u'start_time': 1126259461, u'ligo_angle_sig': None, u'bandwidth': 51.838589, u'snr': 23.4520787991171, u'ligo_angle': None, u'amplitude': 14.099283, u'ligo_axis_ra': 130.921906, u'duration': 0.024773, u'ligo_axis_dec': 4.480799, u'ifos': u'', u'peak_time': None, u'peak_time_ns': None}}, u'nevents': None, u'search': u'AllSky', u'submitter': u'waveburst', u'likelihood': 550.0, u'far_is_upper_limit': False}
 
