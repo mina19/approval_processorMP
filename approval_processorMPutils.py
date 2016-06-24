67c12eb997aab62ae8bba257c72b2832760b3e5c
Mina Cho <min-a.cho@ligo.org>
2016-06-23 23:51:51 -0400

    advreq stuff

--- approval_processorMPutils.py
+++ approval_processorMPutils.py
@@ -265,6 +265,7 @@ def parseAlert(alert):
                 logger.info('{0} -- {1} -- Labeling {2}OPS.'.format(convertTime(), graceid, instrument))
                 g.writeLabel(graceid, '{0}OPS'.format(instrument))
             # notify the advocates
+            #logger.info('{0} -- {1} -- Labeling ADVREQ.'.format(convertTime(), graceid, instrument))
             #g.writeLabel(graceid, 'ADVREQ')
             #os.system('echo \'{0}\' | mail -s \'{1} passed criteria for follow-up\' {2}'.format(advocate_text, graceid, advocate_email))
             # expose event to LV-EM
