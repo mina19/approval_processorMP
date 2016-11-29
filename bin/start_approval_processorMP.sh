9a57680b2828f8cf4bad17c866be97e89abaadf1
Alex Urban <alexander.urban@ligo.org>
2016-11-29 13:48:30 -0800

    how to use the gdb_processor-approval_processorMP script

--- bin/start_approval_processorMP.sh
+++ bin/start_approval_processorMP.sh
@@ -2,17 +2,28 @@
 
 thisDir=${PWD}
 
-cd /home/gracedb.processor/lvalert-dev
-source setup.sh
-
-cd /home/gracedb.processor/lvalert-dev/approval_processorMP/bin
-start_comet
+### lvalert_listenMP version of approval_processorMP
+gdb_processor-approval_processorMP start \
+    --bin    /home/gracedb.processor/opt/bin/ \
+    --config /home/gracedb.processor/opt/etc/lvalert_listenMP-approval_processorMP.ini\
+    --out    /home/gracedb.processor/public_html/monitor/approval_processorMP/files\
 
 cd /home/gracedb.processor/public_html/monitor/approval_processorMP/files
-cp /home/gracedb.processor/lvalert-dev/approval_processorMP/etc/lvalert_listenMP.ini lvalert_listenMP.ini
-cp /home/gracedb.processor/lvalert-dev/approval_processorMP/etc/childConfig-approval_processorMP.ini childConfig-approval_processorMP.ini
+ln -s /home/gracedb.processor/opt/etc/lvalert_listenMP-approval_processorMP.ini lvalert_listenMP-approval_processorMP.ini
+ln -s /home/gracedb.processor/opt/etc/childConfig-approval_processorMP.ini childConfig-approval_processorMP.ini
 
-cd /home/gracedb.processor/lvalert-dev/approval_processorMP/bin
-./lvalert-init_approval_processorMP > /home/gracedb.processor/public_html/monitor/approval_processorMP/files/stdout_stderr.log 2>&1 &
+### restarting things while approval_processorMP is running from lvalert-dev
+#cd /home/gracedb.processor/lvalert-dev
+#source setup.sh
+
+#cd /home/gracedb.processor/lvalert-dev/approval_processorMP/bin
+#start_comet
+
+#cd /home/gracedb.processor/public_html/monitor/approval_processorMP/files
+#ln -s /home/gracedb.processor/lvalert-dev/approval_processorMP/etc/lvalert_listenMP.ini lvalert_listenMP.ini
+#ln -s /home/gracedb.processor/lvalert-dev/approval_processorMP/etc/childConfig-approval_processorMP.ini childConfig-approval_processorMP.ini
+#cp -p stdout_stderr.log stdout_stderr.log_`date +%y-%m-%d_%H:%M`
+#cd /home/gracedb.processor/lvalert-dev/approval_processorMP/bin
+#./lvalert-init_approval_processorMP > /home/gracedb.processor/public_html/monitor/approval_processorMP/files/stdout_stderr.log 2>&1 &
 
 cd ${thisDir}
