c189eace8bd57374987ad82d47377a0f71c3f187
Min-A Cho <min-a.cho@ligo.org>
2017-05-16 21:31:55 -0700

    created the test directory with a pipelineThrottle subdirectory with relevant files for testing

---
+++ test/pipelineThrottle/resetThrottle.sh
@@ -0,0 +1,1 @@
+approvalprocessor_commandMP --node=min-a.cho-Approval_ProcessorMP group,Test pipeline,CWB2G resetThrottle
---
+++ test/pipelineThrottle/startApproval_ProcessorMP.sh
@@ -0,0 +1,1 @@
+lvalert_listenMP -N /home/min-a.cho/lvalert-dev/approval_processorMP/.netrc -s lvalert.cgca.uwm.edu -c /home/min-a.cho/lvalert-dev/approval_processorMP/etc/lvalert_listenMP-approval_processorMP.ini -r pipelineThrottle
