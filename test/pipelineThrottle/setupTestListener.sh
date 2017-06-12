ca497f92794bf43f51eda8c4a1c407bb8df94e0e
Min-A Cho <min-a.cho@ligo.org>
2017-06-12 09:19:01 -0700

    making the resetThrottleTest.sh script that will reset the CBC_gstlal_LowMass pipelineThrottle which is what we test with for pipeline throttle

--- test/pipelineThrottle/setupTestListener.sh
+++ test/pipelineThrottle/setupTestListener.sh
@@ -312,6 +312,9 @@ sed -i -e 's/execfile/#execfile/g' approval_processorMPutils.py
 cd ${APPROVAL_PROCESSORMP_DIR}/test/pipelineThrottle
 echo "${REPO_DIR}" > repoDir.txt 
 
+# Make the resetThrottleTest.sh command so that we can reset the CBC_gstlal_LowMass pipeline
+echo "lvalertTest_commandMP --node=${LIGO_NAME}-test -f ${COMMANDSFILE} group,CBC pipeline,gstlal search,LowMass resetThrottle -v" > resetThrottleTest.sh
+
 # Make it easier for sourcing paths and python paths for testing purposes
 echo "export PYTHONPATH=${LVALERTTEST_DIR}/lib:${PYTHONPATH}
 export PATH=${LVALERTTEST_DIR}/bin:${PATH}
