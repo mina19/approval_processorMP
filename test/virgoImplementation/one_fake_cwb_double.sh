aa8843639a9e5d404bcadd3ee96e53e8a894189c
Min-A Cho <min-a.cho@ligo.org>
2017-07-05 00:11:36 -0700

    added scripts for CWB and gstlal double H1 L1 triggers

--- test/virgoImplementation/one_fake_cwb_double.sh
+++ test/virgoImplementation/one_fake_cwb_double.sh
@@ -5,22 +5,22 @@ REPO_DIR=$(cat repoDir.txt)
 # USAGE:: ./one_fake_gstlal.sh
 FAKE_DB=${REPO_DIR}/approval_processorMP/test/FAKE_DB          # Directory where events will be created
 OUT_DIR=${REPO_DIR}/approval_processorMP/test/OUT_DIR          # temporary directory to store upload files
-CONFIG_FILE=${REPO_DIR}/approval_processorMP/test/virgoImplementation/gstlal.ini_1             # config file of the event created, will be written by script
+CONFIG_FILE=${REPO_DIR}/approval_processorMP/test/virgoImplementation/cwb.ini_1             # config file of the event created, will be written by script
 NUM_EVENTS=1
 
 # Event details
-GROUP="CBC"
-PIPELINE="gstlal"
-SEARCH="LowMass"
-INSTRUMENTS="H1,L1"
+GROUP="Burst"
+PIPELINE="CWB"
+SEARCH="AllSky"
+INSTRUMENTS="H1,L1,V1"
 FAR=1e-8		# Event FAR
 HUMANS=1		# Add humans section?
 HUMAN_RESPONSE=1	# 0 for ADVNO, 1 for ADVOK
 SEGDB2GRCDB=0		# Add segdb2grcdb section?
-VIRGODQ=0		# Add VirgoDQ section?
+VIRGODQ=1		# Add VirgoDQ section?
 VIRGODQRESPONSE=1	# 0 for IS vetoed (fail); 1 for IS NOT vetoed (pass)
-VIRGOINJ=0		# 0 for DID NOT FIND injections; 1 for DID FIND injections
-IDQ=0			# Add idq section?
+VIRGOINJ=1		# 0 for DID NOT FIND injections; 1 for DID FIND injections
+IDQ=1			# Add idq section?
 IDQ_RESPONSE=1		# Pass/ fail criteria AP's IDQ threshold
 SKYMAPS=1		# Adds various skymaps?
 LVEM=1			# Add lvem tag?
