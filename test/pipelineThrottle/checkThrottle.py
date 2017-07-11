14731e880490b74b2552eb04c03dfffd7561d55f
Min-A Cho <min-a.cho@ligo.org>
2017-07-11 13:10:45 -0700

    script to check simulateThrottle.sh

---
+++ test/pipelineThrottle/checkThrottle.py
@@ -0,0 +1,20 @@
+from eventDictClassMethods import *
+
+repoDirFile = open('repoDir.txt', 'r')
+repoDir = repoDirFile.read()
+repoDirFile.close()
+
+repoDir = repoDir.strip('\n')
+FAKEDB_DIR = repoDir + '/approval_processorMP/test/FAKE_DB'
+g = initGraceDb(FAKEDB_DIR)
+
+EM_Throttled = []
+query_string = 'EM_Throttled'
+EM_ThrottledEvents = g.events(query_string)
+
+for event in EM_ThrottledEvents:
+    graceid = event['graceid']
+    EM_Throttled.append(graceid)
+
+print('EM_Throttled events: {0}'.format(EM_READY))
+print('Only events G000000, G000001, and G000002 should be EM_Throttled')
