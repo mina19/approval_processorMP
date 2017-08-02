eeef64c9b39958d547e177f3d1adab3f4cf306c9
Min-A Cho <min-a.cho@ligo.org>
2017-08-01 23:50:47 -0700

    fixed so the scripts run

--- test/grouper/simulate_H1L1_events.sh
+++ test/grouper/simulate_H1L1_events.sh
@@ -3,7 +3,7 @@
 REPO_DIR=$(cat repoDir.txt)
 
 # USAGE:: ./one_fake_gstlal.sh
-FAKE_DB=${REPO_DIR}/approval_processorMP/test/FAKE_DB          # Directory where events will be created
+FAKE_DB=${REPO_DIR}/approval_processorMP/test/FAKE_DB/          # Directory where events will be created
 OUT_DIR=${REPO_DIR}/approval_processorMP/test/OUT_DIR          # temporary directory to store upload files
 CONFIG_FILE=${REPO_DIR}/approval_processorMP/test/virgoImplementation/gstlal.ini_1             # config file of the event created, will be written by script
 NUM_EVENTS=2 # we are creating five events for testing grouper -- 2 cwb events and 3 gstlal events
@@ -13,7 +13,6 @@ GPSTIME=0 # this will be edited when we call this script with tconvert now
 
 ${REPO_DIR}/lvalertTest/bin/simulate.py \
         --num-events=${NUM_EVENTS} \
-        #--far=${FAR} \
         --gracedb-url=${FAKE_DB} \
         --instruments=${INSTRUMENTS} \
         --output-dir=${OUT_DIR} \
