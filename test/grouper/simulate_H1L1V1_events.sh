30612b5b96bce0f19d7627fb1d369f843d06d37a
Min-A Cho <min-a.cho@ligo.org>
2017-08-01 23:04:12 -0700

    scripts that makes calls to simulate.py

---
+++ test/grouper/simulate_H1L1V1_events.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+REPO_DIR=$(cat repoDir.txt)
+
+# USAGE:: ./one_fake_gstlal.sh
+FAKE_DB=${REPO_DIR}/approval_processorMP/test/FAKE_DB          # Directory where events will be created
+OUT_DIR=${REPO_DIR}/approval_processorMP/test/OUT_DIR          # temporary directory to store upload files
+CONFIG_FILE=${REPO_DIR}/approval_processorMP/test/virgoImplementation/gstlal.ini_1             # config file of the event created, will be written by script
+NUM_EVENTS=2 # we are creating five events for testing grouper -- 2 cwb events and 3 gstlal events
+EVENT_RATE=10000 # the rate for simulating events specified in Hz
+INSTRUMENTS="H1,L1,V1"
+GPSTIME=0 # this will be edited when we call this script with tconvert now
+
+${REPO_DIR}/lvalertTest/bin/simulate.py \
+        --num-events=${NUM_EVENTS} \
+        #--far=${FAR} \
+        --gracedb-url=${FAKE_DB} \
+        --instruments=${INSTRUMENTS} \
+        --output-dir=${OUT_DIR} \
+        --event-rate=${EVENT_RATE} \
+        -s \
+        -v \
+        cwb.ini_2 gstlal.ini_3 \
+        --start-time=${GPSTIME} 
