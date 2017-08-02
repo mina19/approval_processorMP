e7e0ac0cec5abe46dd9ae598cee3ae3dca1436bc
Min-A Cho <min-a.cho@ligo.org>
2017-08-01 23:36:55 -0700

    script to create config files and alter simulating scripts to have similar gpstimes and then actually create triggers

---
+++ test/grouper/simulate_group.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# call the scripts that create the config files
+./one_fake_cwb_double.sh
+./one_fake_cwb_virgodq.sh
+./one_fake_gstlal_double.sh
+./one_fake_gstlal_double_V1.sh
+./one_fake_gstlal_triple.sh
+
+GPSTIME="`tconvert now`"
+echo $GPSTIME
+
+# fixing the simulate.py calling scripts to have the same gpstime so that the simulated events have similar gpstimes
+sed -i -e 's/GPSTIME=0/GPSTIME='$GPSTIME'/g' simulate_H1L1_events.sh
+sed -i -e 's/GPSTIME=0/GPSTIME='$GPSTIME'/g' simulate_H1V1_event.sh
+sed -i -e 's/GPSTIME=0/GPSTIME='$GPSTIME'/g' simulate_H1L1V1_events.sh
+
+# now calling the scripts to simulate the triggers with the altered gpstimes
+./simulate_H1L1_events.sh
+./simulate_H1L1V1_events.sh
+./simulate_H1V1_event.sh
+
